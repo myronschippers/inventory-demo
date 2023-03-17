66f55de9dbe5cbe77c25cbdcecf0a465aae36087
Myron R Schippers Jr <dubtwee@gmail.com>
2023-03-16 20:20:42 -0400

    adding code comments for future content

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!!!');
 });
 
+// CRUD = DB => Server
+// C: Create => post
+// R: Read   => get
+// U: Update => put
+// D: Delete => delete
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
