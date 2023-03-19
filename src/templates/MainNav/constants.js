4a1b0d4fab27062d7760754af69b13b9df5048c2
Myron R Schippers Jr <dubtwee@gmail.com>
2023-03-19 00:31:48 -0400

    creating a basic layout for page content and removing boilerplate styles

---
+++ src/templates/MainNav/constants.js
@@ -0,0 +1,18 @@
+export const NAV_LIST = [
+  {
+    text: 'Home',
+    path: '/',
+  },
+  {
+    text: 'Inventory',
+    path: '/inventory',
+  },
+  {
+    text: 'Add Inventory',
+    path: '/add-inventory',
+  },
+  {
+    text: 'Remove Inventory',
+    path: '/remove-inventory',
+  },
+];
