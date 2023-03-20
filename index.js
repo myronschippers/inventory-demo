307001798128ab20a2c2a0e88a43761eb1039955
Myron R Schippers Jr <dubtwee@gmail.com>
2023-03-20 19:15:14 -0400

    making inverntory GET route for API

--- index.js
+++ index.js
@@ -4,18 +4,47 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+// Make full '/inventory' route
+const INVENTORY = [
+  {
+    name: 'Chips',
+    type: 'food',
+    status: 'home',
+    quantity: 3,
+  },
+  {
+    name: 'Dr. Tran',
+    type: 'movie',
+    status: 'home',
+    quantity: 1,
+  },
+  {
+    name: 'Skies',
+    type: 'recreation',
+    status: 'home',
+    quantity: 2,
+  },
+];
+
 app.get('/', (req, res) => {
   res.send('Hello World!!!');
 });
 
 // CRUD = DB => Server
 // C: Create => post
-// R: Read   => get
+// R: Read   => get "GET"
 // U: Update => put
 // D: Delete => delete
 
-// Make full '/inventory' route
-const inventory = [{ name: '' }];
+// GET for inventory
+app.get('/inventory', (req, res) => {
+  res.send(INVENTORY);
+});
+
+// thought experiment LAST ITEM
+app.get('/inventory/last-item', (req, res) => {
+  res.send(INVENTORY[INVENTORY.length - 1]);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
