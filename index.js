c50b57f27572a986c148654ee4710879a1bcfce8
Myron R Schippers Jr <dubtwee@gmail.com>
2023-03-20 20:28:58 -0400

    created a PUT to update existing inventory item

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ const PORT = 8080;
 // Make full '/inventory' route
 const INVENTORY = [
   {
+    id: 1,
     name: 'Chips',
     type: 'food',
     status: 'home',
     quantity: 3,
   },
   {
+    id: 2,
     name: 'Dr. Tran',
     type: 'movie',
     status: 'home',
     quantity: 1,
   },
   {
+    id: 3,
     name: 'Skies',
     type: 'recreation',
     status: 'home',
@@ -62,6 +65,33 @@ app.post('/inventory', (req, res) => {
   }
 
   res.sendStatus(201);
+});
+
+app.put('/inventory/:id', (req, res) => {
+  try {
+    const newData = req.body;
+    const inventoryId = parseInt(req.params.id);
+
+    for (let i = 0; i < INVENTORY.length; i++) {
+      const invItem = INVENTORY[i];
+
+      if (invItem.id === inventoryId) {
+        if (typeof newData.name !== 'string') {
+          res.status(500);
+          res.send('Name value must be a string');
+        }
+
+        INVENTORY[i] = {
+          ...invItem,
+          ...newData,
+        };
+      }
+    }
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(500);
+  }
 });
 
 app.listen(PORT, () => {
